c06067dba666756e82f883c84f549f2e298d3136
sambulo senda <sambulosenda@sambulos-MacBook-Pro.local>
2022-05-22 20:10:43 +0100

    home screen all post now using apollo

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import Navigation from './src/navigation';
 import Amplify, { photoPlaceholderIcon } from 'aws-amplify';
 import config from './src/aws-exports';
 import AuthContextProvider from './src/contexts/AuthContext';
+import Client from './src/apollo/Client';
 
 Amplify.configure(config);
 
 const App = () => {
   return (
     <AuthContextProvider>
-      <Navigation />
+      <Client><Navigation /></Client>
+      
     </AuthContextProvider>
   );
 };
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,28 @@
-import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+  DataDetectorTypes,
+} from 'react-native';
 
 import FeedPost from '../../components/FeedPost/FeedPost';
 
 import { API, graphqlOperation } from 'aws-amplify';
 import { useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { ListPostsQuery, ListPostsQueryVariables } from '../../API';
+import ApiErrorMessage from '../../components/ApiErrorMessage/ApiErrorMessage';
 
-export const listPosts = /* GraphQL */ `
+export const listPosts = gql`
   query ListPosts($filter: ModelPostFilterInput, $limit: Int, $nextToken: String) {
     listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
+        
         description
         image
         images
@@ -46,22 +59,28 @@ export const listPosts = /* GraphQL */ `
 `;
 
 const HomeScreen = (props) => {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
+  // const fetchAllPosts = async () => {
+  //   const response = await API.graphql(graphqlOperation(listPosts));
+  //   console.log(response);
+  //   setPosts(response.data.listPosts.items);
+  // };
 
-  const fetchAllPosts = async () => {
-    const response = await API.graphql(graphqlOperation(listPosts));
-    console.log(response);
-    setPosts(response.data.listPosts.items);
-  };
+  // useEffect(() => {
+  //   fetchAllPosts();
+  // }, []);
 
-  useEffect(() => {
-    fetchAllPosts();
-  }, []);
+  const { data, loading, error } = useQuery<ListPostsQuery, ListPostsQueryVariables>(listPosts);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <ApiErrorMessage title='Error fetching posts' message={error.message} />;
+
+  const posts = data?.listPosts?.items || [];
 
   return (
     <FlatList
       data={posts}
-      renderItem={({ item }) => <FeedPost post={item} />}
+      renderItem={({ item }) => item && <FeedPost post={item} />}
       showsVerticalScrollIndicator={false}
     />
   );
